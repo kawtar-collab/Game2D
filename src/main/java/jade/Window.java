9cf72cd49961fc89b990a2655911b58f10d2c88a
kawtar-collab <kawtarkhallouq073@gmail.com>
2023-12-14 00:39:07 +0100

    Adding Scene Manager

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -3,6 +3,8 @@ package jade;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
+
+import util.Time;
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 
 import static org.lwjgl.glfw.GLFW.*;
@@ -12,11 +14,14 @@ import static org.lwjgl.system.MemoryUtil.NULL;
 public class Window {
     private int width, height;
     private String title;
-    private long glfwWindow;
+    public long glfwWindow;
 
-    private float r, g, b, a;
+    public float r, g, b, a;
+
     private boolean fadeToBlack = false;
     private static Window window = null;
+
+    private static Scene currentScene;
 
     private Window() {
         this.width = 1920;
@@ -26,6 +31,22 @@ public class Window {
         b = 1;
         g = 1;
         a = 1;
+    }
+
+
+    public static void changeScene(int newScene) {
+        switch (newScene) {
+            case 0:
+                currentScene = new LevelEditorScene();
+                //currentScene.init();
+                break;
+            case 1:
+                currentScene = new LevelScene();
+                break;
+            default:
+                assert false : "Unknown scene '" + newScene + "'";
+                break;
+        }
     }
 
     public static Window get() {
@@ -94,28 +115,32 @@ public class Window {
         // creates the GLCapabilities instance and makes the OpenGL
         // bindings available for use.
         GL.createCapabilities();
+
+        Window.changeScene(0);
     }
 
     public void loop() {
+        float beginTime = Time.getTime();
+        float endTime;
+        float dt = -1.0f;
+
         while (!glfwWindowShouldClose(glfwWindow)) {
             // Poll events
             glfwPollEvents();
 
-            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
             glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
 
-            if (fadeToBlack) {
-                r = Math.max(r - 0.01f, 0);
-                g = Math.max(g - 0.01f, 0);
-                b = Math.max(b - 0.01f, 0);
-            }
-
-            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
-                fadeToBlack = true;
+            if (dt >= 0) {
+                    currentScene.update(dt);
             }
 
             glfwSwapBuffers(glfwWindow);
+
+            endTime = Time.getTime();
+            dt = endTime - beginTime;
+            beginTime = endTime;
         }
     }
 }
